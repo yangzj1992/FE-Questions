2df1deef2f4706f2fd621d01f65ac83eed7c1784
yangzj1992 <yangzj1992@qq.com>
2016-04-24 19:04:33 +0800

    feat: basic build

---
+++ leetcode/Algorithms/2.Add Two Numbers/index.js
@@ -0,0 +1,28 @@
+// Source : https://leetcode.com/problems/add-two-numbers/
+// Author : yangzj1992
+
+/**
+ * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+
+Subscribe to see which companies asked this question
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  
+};
